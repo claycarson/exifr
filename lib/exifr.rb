50e46bf85a3ba79eb392ad685650015d3b81ffa2
Clay Carson <me@claycarson.net>
2021-03-04 14:51:53 -0600

    Update exifr.rb

--- lib/exifr.rb
+++ lib/exifr.rb
@@ -1,12 +1,11 @@
-# Copyright (c) 2006-2020 - R.W. van 't Veer
-
-require 'logger'
+# Copyright (c) 2006, 2007, 2008, 2009, 2010, 2011 - R.W. van 't Veer
 
 module EXIFR
+  class BadURL < StandardError; end
   class MalformedImage < StandardError; end
   class MalformedJPEG < MalformedImage; end
   class MalformedTIFF < MalformedImage; end
-
-  class << self; attr_accessor :logger; end
-  self.logger = Logger.new(STDERR)
 end
+
+require 'exifr/jpeg'
+require 'exifr/tiff'
